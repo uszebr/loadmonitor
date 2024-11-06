9e53e09df5c7596487127176396e652393f59416
uszebr <uszebr@gmail.com>
2024-11-06 07:17:30 -0500

    add loadmanager page; loadmanager templ, handler, favicon

--- inner/util/ginutil/ginutil.go
+++ inner/util/ginutil/ginutil.go
@@ -5,7 +5,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// util for easier handling Templ components with Gin
+// video about gin/ https://github.com/matheusgomes28/urchin/blob/main/migrations/20240115222144_add_posts_table.sql
+// Render util for easier handling Templ components with Gin
 func Render(c *gin.Context, status int, template templ.Component) error {
 	c.Status(status)
 	return template.Render(c.Request.Context(), c.Writer)
